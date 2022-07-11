289936a6f747c6f09325ad43c11ee563c9948f97
sanilnvidia <sanilr@nvidia.com>
2022-07-11 15:34:50 -0400

    updated SLPA

--- SLPA.cu
+++ SLPA.cu
@@ -4,9 +4,9 @@
 #include <curand_kernel.h>
 #include <curand.h>
 #include <stdio.h>
+#include <math.h>
 
-
-#define N 1
+#define N 32
 #define B 1
 
 
@@ -27,11 +27,19 @@ typedef struct triples {
 }triples;
 
 //update labellist with random label
-__global__ void SLPASpeaker(int * row, int *col, int *val, int * memnnz, triples *mem, triples *labellist, int n, int T, curandState *states, uint64_t seed) {
+__global__ void SLPASpeaker(int * row, int *col, int *val, int * memnnz, int * row_id, triples *mem, triples *labellist, int n, int T, curandState *states, uint64_t seed) {
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
     curand_init(seed, tid, 0, &states[tid]);
     for(int i = tid; i < row[n]; i+=N*B) { 
-        int offset = ((int)curand_uniform(&states[tid]) %(memnnz[i]+1)); 
+        float rand = curand_uniform(&states[tid]);
+        float rand2 = rand * memnnz[col[i]];
+        int offset = (int)truncf(rand2);
+        // if(49 <= i && i < 56) {
+        // printf("memnnz is %d\n", memnnz[col[i]]);
+        // printf("original rand %f and new rand %f\n", rand, rand2);
+        // printf("The offset for %d is %d\n", i, offset);
+        // }
+        //int offset = ((int)curand_uniform(&states[tid]) %(memnnz[i]+1)); 
         labellist[i] = mem[col[i]*n + offset];
         labellist[i].val = 1;
         labellist[i].iter = T;
@@ -45,38 +53,54 @@ __global__ void SLPAListener1(int * row, int *col, int *val, triples *labellist,
     for(int i = tid; i < row[n]; i+=N*B) {
         //get offsets of specific lists
         int offset1 = row[row_id[i]];
-        int offset2 = row[row_id[i+1]];
+        int offset2 = row[row_id[i]+1];
         __shared__ int count[N];
-        count[i%N] = 0;
-        __shared__ int startval[N];
-        startval[i%N] = labellist[i].key;
+        count[tid%N] = 0;
         //go over list and count how often that label exists in the list
         for(int j = offset1; j < offset2; j++) {
-            if(startval[i%N] == labellist[j].key)
-                count[i%N]++; 
+            if(labellist[i].key == labellist[j].key)
+                count[tid%N]++; 
         }
-        labellist[i].val = count[i%N];
+        labellist[i].val = count[tid%N];
     }
 }
+
 //get most seen label 
-__global__ void SLPAListener2(int * row, int *col, int *val, triples *labellist, int n) {
-    for(int i = blockIdx.x; i < n; i+=blockDim.x) {
-        int offset1 = row[i];
-        int offset2 = row[i+1];
-        int x = offset2-offset1;
-        while(threadIdx.x < x/2) {
-            labellist[threadIdx.x] = (labellist[threadIdx.x].val > labellist[threadIdx.x+(x/2)].val) ? labellist[threadIdx.x] : 
-                labellist[threadIdx.x+(x/2)];
-            x = x/2;
-            __syncthreads();
+__global__ void SLPAListener2(int * row, int *col, int *val, triples *labellist, int n, int * row_id) {
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    for(int i = tid; i < row[n]; i++) {
+        if(labellist[row[row_id[i]]].val < labellist[i].val) {
+            atomicCAS(&(labellist[row[row_id[i]]].key), labellist[row[row_id[i]]].key, labellist[i].key);
+            atomicCAS(&(labellist[row[row_id[i]]].val), labellist[row[row_id[i]]].val, labellist[i].val);
+            atomicCAS(&(labellist[row[row_id[i]]].iter), labellist[row[row_id[i]]].iter, labellist[i].iter);
         }
     }
 }
+
+// //get most seen label 
+// __global__ void SLPAListener2(int * row, int *col, int *val, triples *labellist, int n) {
+//     for(int i = blockIdx.x; i < n; i+=blockDim.x) {
+//         int offset1 = row[i];
+//         int offset2 = row[i+1];
+//         int x = offset2-offset1;
+//         if(i == 12) {
+//             printf("%d\n", x);
+//         }
+//         while(threadIdx.x < x/2) {
+//             if(i == 12)
+//             printf("%d\n has value %d %d and compares with %d %d", threadIdx.x, labellist[threadIdx.x].key, labellist[threadIdx.x].val, labellist[threadIdx.x+(x/2)].key, labellist[threadIdx.x+(x/2)].val);
+//             labellist[threadIdx.x] = (labellist[threadIdx.x].val > labellist[threadIdx.x+(x/2)].val) ? labellist[threadIdx.x] : 
+//                 labellist[threadIdx.x+(x/2)];
+//             x = x/2;
+//             __syncthreads();
+//         }
+//     }
+// }
 //update listener per node memory
 __global__ void SLPAListener3(int * row, int *col, int *val, int * memnnz, triples *mem, triples *labellist, int n, int T) {
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
     for(int i = tid; i < n; i+= N*B) {
-        if(memnnz[i] < n) { //wiil be d+1 in the future 
+        //if(memnnz[i] < n) { //wiil be d+1 in the future 
             int flag = 0;
             for(int j = 0; j < n; j++) {
                 if(mem[n*i+j].key == labellist[row[i]].key) {
@@ -84,30 +108,43 @@ __global__ void SLPAListener3(int * row, int *col, int *val, int * memnnz, tripl
                     flag = 1;
                 }
             }
-            if(flag == 0) {
+            if(flag == 0 && memnnz[i] < n) {
                 mem[n*i+memnnz[i]] = labellist[row[i]];
                 mem[n*i+memnnz[i]].val = 1;
                 memnnz[i]++;
             }
-        }
-        else{
-            int lowcount = 1000000000;
-            int index = -1;
-            for(int j = 0; j < n; j++) {
-                if(mem[n*i+j].val < lowcount) {
-                    index = j;
-                    lowcount = mem[n*i+j].val;
+            else if(flag == 0 && memnnz[i] >=n) {
+                int lowcount = 1000000000;
+                int index = -1;
+                for(int j = 0; j < n; j++) {
+                    if(mem[n*i+j].val < lowcount) {
+                        index = j;
+                        lowcount = mem[n*i+j].val;
+                    }
                 }
+                mem[n*i+index] = labellist[row[i]];
+                mem[n*i+index].val = 1; 
             }
-            mem[n*i+index] = labellist[row[i]];
-            mem[n*i+index].val = 1; 
-        } 
+        //}
+       //else{
+            // int lowcount = 1000000000;
+            // int index = -1;
+            // for(int j = 0; j < n; j++) {
+            //     if(mem[n*i+j].val < lowcount) {
+            //         index = j;
+            //         lowcount = mem[n*i+j].val;
+            //     }
+            // }
+            // mem[n*i+index] = labellist[row[i]];
+            // mem[n*i+index].val = 1; 
+       //} 
     }
 }
 
-__global__ void SLPAPostProcess(triples *mem, int n, int T, int r) {
+__global__ void SLPAPostProcess(triples *mem, int n, int T, float r) {
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
-    for(int i = tid; i < n*n; i++) { //d+1
+    for(int i = tid; i < n*n; i+=N*B) { //d+1
+        //printf("%d %f\n", mem[i].val, T*r);
         if(mem[i].val < T*r) {
             mem[i].key = -1;
             mem[i].val = 0;
@@ -134,85 +171,306 @@ __global__ void SLPAPostProcess(triples *mem, int n, int T, int r) {
 
 
 int main() {
-    /* 0 1 0
-       1 0 0
-       1 1 0
 
-    */
-    int n = 3;
+    //Example graph taken from paper https://webdocs.cs.ualberta.ca/~zaiane/postscript/cason09.pdf
+
+
+    int n = 13;
     int * row;
     int *col, *val, *row_id, *memnnz;
     triples *mem, *labellist;
     std::cout << "malloc call\n";
-    gpuErrchk(cudaMallocManaged(&row, 4 * sizeof(int)));
-    // cudaMallocManaged(&col, 4 * sizeof(int));
-    // cudaMallocManaged(&val, 4 * sizeof(int));
-    // cudaMallocManaged(&memnnz, 3 * sizeof(int));
-    // cudaMallocManaged(&row_id, 3 * sizeof(int));
-    // cudaMallocManaged(&mem, n*n*sizeof(triples));
-    // cudaMallocManaged(&labellist, n*sizeof(triples));
+    gpuErrchk(cudaMallocManaged(&row, n * sizeof(int)));
+    cudaMallocManaged(&col, 56 * sizeof(int));
+    cudaMallocManaged(&val, 56 * sizeof(int));
+    cudaMallocManaged(&memnnz, n * sizeof(int));
+    cudaMallocManaged(&row_id, n * sizeof(int));
+    cudaMallocManaged(&mem, n*n*sizeof(triples));
+    cudaMallocManaged(&labellist, n*sizeof(triples));
     
     row[0] = 0;
-    std::cout << row[0] << "\n";
-    exit(0);
-    row[1] = 1;
-    row[2] = 2;
-    row[3] = 4;
+    row[1] = 3;
+    row[2] = 7;
+    row[3] = 12;
+    row[4] = 17;
+    row[5] = 21;
+    row[6] = 26;
+    row[7] = 31;
+    row[8] = 34;
+    row[9] = 38;
+    row[10] = 42;
+    row[11] = 46;
+    row[12] = 49;
+    row[13] = 56;
 
     col[0] = 1;
-    col[1] = 0;
-    col[2] = 1;
-    col[3] = 1;
+    col[1] = 2;
+    col[2] = 3;
+    col[3] = 0;
+    col[4] = 2;
+    col[5] = 3;
+    col[6] = 12;
+    col[7] = 0;
+    col[8] = 1;
+    col[9] = 3;
+    col[10] = 6;
+    col[11] = 12;
+    col[12] = 0;
+    col[13] = 1;
+    col[14] = 2;
+    col[15] = 8;
+    col[16] = 12;
+    col[17] = 5;
+    col[18] = 6;
+    col[19] = 7;
+    col[20] = 12;
+    col[21] = 4;
+    col[22] = 6;
+    col[23] = 7;
+    col[24] = 9;
+    col[25] = 12;
+    col[26] = 2;
+    col[27] = 4;
+    col[28] = 5;
+    col[29] = 7;
+    col[30] = 12;
+    col[31] = 4;
+    col[32] = 5;
+    col[33] = 6;
+    col[34] = 3;
+    col[35] = 9;
+    col[36] = 10;
+    col[37] = 11;
+    col[38] = 5;
+    col[39] = 8;
+    col[40] = 10;
+    col[41] = 11;
+    col[42] = 8;
+    col[43] = 9;
+    col[44] = 11;
+    col[45] = 12;
+    col[46] = 8;
+    col[47] = 9;
+    col[48] = 10;
+    col[49] = 1;
+    col[50] = 2;
+    col[51] = 3;
+    col[52] = 4;
+    col[53] = 5;
+    col[54] = 6;
+    col[55] = 10;
 
-    val[0] = 1;
-    val[1] = 1;
-    val[2] = 1;
-    val[3] = 1;
+    val[0] = 1;    
+    val[1] = 1;    
+    val[2] = 1;    
+    val[3] = 1;    
+    val[4] = 1;    
+    val[5] = 1;    
+    val[6] = 1;
+    val[7] = 1;    
+    val[8] = 1;    
+    val[9] = 1;    
+    val[10] = 1;
+    val[11] = 1;
+    val[12] = 1;
+    val[13] = 1;
+    val[14] = 1;
+    val[15] = 1;
+    val[16] = 1;
+    val[17] = 1;
+    val[18] = 1;
+    val[19] = 1;
+    val[20] = 1;
+    val[21] = 1;
+    val[22] = 1;
+    val[23] = 1;
+    val[24] = 1;
+    val[25] = 1;
+    val[26] = 1;
+    val[27] = 1;
+    val[28] = 1;
+    val[29] = 1;
+    val[30] = 1;
+    val[31] = 1;
+    val[32] = 1;
+    val[33] = 1;
+    val[34] = 1;
+    val[35] = 1;
+    val[36] = 1;
+    val[37] = 1;
+    val[38] = 1;
+    val[39] = 1;
+    val[40] = 1;
+    val[41] = 1;
+    val[42] = 1;
+    val[43] = 1;
+    val[44] = 1;
+    val[45] = 1;
+    val[46] = 1;
+    val[47] = 1;
+    val[48] = 1;
+    val[49] = 1;
+    val[50] = 1;
+    val[51] = 1;
+    val[52] = 1;
+    val[53] = 1;
+    val[54] = 1;
+    val[55] = 1;
 
-    row_id[0] = 0;
-    row_id[1] = 1;
-    row_id[2] = 1;
-    row_id[3] = 2;
+    row_id[0] = 0;    
+    row_id[1] = 0;    
+    row_id[2] = 0;    
+    row_id[3] = 1;    
+    row_id[4] = 1;    
+    row_id[5] = 1;    
+    row_id[6] = 1;
+    row_id[7] = 2;    
+    row_id[8] = 2;    
+    row_id[9] = 2;    
+    row_id[10] = 2;
+    row_id[11] = 2;
+    row_id[12] = 3;
+    row_id[13] = 3;
+    row_id[14] = 3;
+    row_id[15] = 3;
+    row_id[16] = 3;
+    row_id[17] = 4;
+    row_id[18] = 4;
+    row_id[19] = 4;
+    row_id[20] = 4;
+    row_id[21] = 5;
+    row_id[22] = 5;
+    row_id[23] = 5;
+    row_id[24] = 5;
+    row_id[25] = 5;
+    row_id[26] = 6;
+    row_id[27] = 6;
+    row_id[28] = 6;
+    row_id[29] = 6;
+    row_id[30] = 6;
+    row_id[31] = 7;
+    row_id[32] = 7;
+    row_id[33] = 7;
+    row_id[34] = 8;
+    row_id[35] = 8;
+    row_id[36] = 8;
+    row_id[37] = 8;
+    row_id[38] = 9;
+    row_id[39] = 9;
+    row_id[40] = 9;
+    row_id[41] = 9;
+    row_id[42] = 10;
+    row_id[43] = 10;
+    row_id[44] = 10;
+    row_id[45] = 10;
+    row_id[46] = 11;
+    row_id[47] = 11;
+    row_id[48] = 11;
+    row_id[49] = 12;
+    row_id[50] = 12;
+    row_id[51] = 12;
+    row_id[52] = 12;
+    row_id[53] = 12;
+    row_id[54] = 12;
+    row_id[55] = 12;
 
-
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < 56; i++) {
         labellist[i].key = -1;
         labellist[i].val = 0;
+    }
+
+    for(int i = 0; i < n; i++) {
         memnnz[i] = 1;
         for(int j = 0; j < n; j++) {
             mem[i*n + j].key = i;
             mem[i*n + j].val = 1;
         }
     }
-    int T = 5;
-    float r = 0.3;
+    int T = 100;
+    float r = 0.2;
     curandState *dev_random;
     cudaMallocManaged(&dev_random, N*B*sizeof(curandState));
 
     for(int i = 1; i < T; i++) {
+        //  if(i%10 == 0) {
+        // std::cout << "memnnz values per vertex:\n";
+        // for(int j = 0; j < n; j++) {
+        //     std::cout << j << ":" << memnnz[j] << "  ";
+        // }
+        // std::cout << "\n";
+        //  }
         //speaker rule
-        SLPASpeaker<<<B,N>>>(row,col,val, memnnz, mem, labellist, n, T, dev_random, time(NULL));
+        std::cout << time(NULL)+i << "\n";
+        SLPASpeaker<<<B,N>>>(row,col,val, memnnz, row_id, mem, labellist, n, T, dev_random, time(NULL)+i);
         cudaDeviceSynchronize();
+        if(i%10 == 0) {
+            std::cout << "finished iteration after speaker    " << i << "\n";
+            for(int k = 12; k < n; k++) {
+                std::cout << "Row " << k << " labelist\n";
+                for(int j = row[k]; j < row[k+1]; j++) {
+                    std::cout << labellist[j].key << " " << labellist[j].val << "\t\t\t";
+                }
+                std::cout << "\n";
+            }
+        }
         //label frequency
         SLPAListener1<<<B,N>>>(row,col,val, labellist, row_id, n);
         cudaDeviceSynchronize();
+        // if(i%10 == 0) {
+        //     std::cout << "finished iteration after listener 1    " << i << "\n";
+        //     for(int k = 12; k < n; k++) {
+        //         std::cout << "Row " << k << " labelist\n";
+        //         for(int j = row[k]; j < row[k+1]; j++) {
+        //             std::cout << labellist[j].key << " " << labellist[j].val << "\t\t\t";
+        //         }
+        //         std::cout << "\n";
+        //     }
+        // }
         //max frequency
-        SLPAListener2<<<B,N>>>(row,col,val, labellist, n);
+        SLPAListener2<<<B,N>>>(row,col,val, labellist, n, row_id);
         cudaDeviceSynchronize();
+        //  if(i%10 == 0) {
+        //     std::cout << "finished iteration after listener 2    " << i << "\n";
+        //     for(int k = 12; k < n; k++) {
+        //         std::cout << "Row " << k << " labelist\n";
+        //         for(int j = row[k]; j < row[k+1]; j++) {
+        //             std::cout << labellist[j].key << " " << labellist[j].val << "\t\t\t";
+        //         }
+        //         std::cout << "\n";
+        //     }
+        // }
         //memory update
         SLPAListener3<<<B,N>>>(row,col,val, memnnz, mem, labellist, n, T);
         cudaDeviceSynchronize();
+        //std::cout << "Finished iteration " << i << "\n";
+        if(i%10 == 0) {
+            std::cout << "finished iteration " << i << "\n";
+            for(int k = 0; k < n; k++) {
+                std::cout << "vertex " << k << "\t";
+                for(int j = 0; j < memnnz[k]; j++) {
+                    std::cout << mem[n*k+j].key << " " << mem[n*k+j].val << "\t";
+                }
+                std::cout << "\n";
+            }
+        }
     }
+    
     SLPAPostProcess<<<B,N>>>(mem, n, T, r);
     cudaDeviceSynchronize();
     //SLPA<<<B,N>>>(row,col,val,mem, n, T,r, dev_random);
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+        int count = 0;
+        std::cout << "Vertex " << i << " is in communities ";
+        for(int j = 0; j < memnnz[i]; j++) {
             if(mem[i*n+j].key != -1) {
                 std::cout << mem[i*n+j].key << " ";
+                count++;
             }
-            std::cout << "overlap\n";
         }
+        if(count > 1)
+            std::cout << "overlap\n";
+        else
+            std::cout << " only community\n";
     }
     std::cout << "\n";
     return 0;
