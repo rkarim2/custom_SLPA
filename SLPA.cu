a2816a3b1b75065a75f6220fb90e168e26c852f0
sanilnvidia <sanilr@nvidia.com>
2022-07-27 16:41:57 -0400

    added timing

--- SLPA.cu
+++ SLPA.cu
@@ -313,10 +313,10 @@ int main(int argc, char *argv[]) {
     std::cout << "malloc call\n";
 
 
-    //convertToCSR(argv, &row, &col, n, cols);
+    convertToCSR(argv, &row, &col, n, cols);
 
-    cudaMallocManaged(&row, (n+1) * sizeof(int));
-    cudaMallocManaged(&col, cols * sizeof(int));
+    // cudaMallocManaged(&row, (n+1) * sizeof(int));
+    // cudaMallocManaged(&col, cols * sizeof(int));
     cudaMallocManaged(&val, cols * sizeof(int));
     cudaMallocManaged(&memnnz, n * sizeof(int));
     cudaMallocManaged(&row_id, cols * sizeof(int));
@@ -329,198 +329,198 @@ int main(int argc, char *argv[]) {
     // }
     // std::cout << "\n";
     //exit(0);
-    row[0] = 0;
-    row[1] = 3;
-    row[2] = 7;
-    row[3] = 12;
-    row[4] = 17;
-    row[5] = 21;
-    row[6] = 26;
-    row[7] = 31;
-    row[8] = 34;
-    row[9] = 38;
-    row[10] = 42;
-    row[11] = 46;
-    row[12] = 49;
-    row[13] = 56;
+    // row[0] = 0;
+    // row[1] = 3;
+    // row[2] = 7;
+    // row[3] = 12;
+    // row[4] = 17;
+    // row[5] = 21;
+    // row[6] = 26;
+    // row[7] = 31;
+    // row[8] = 34;
+    // row[9] = 38;
+    // row[10] = 42;
+    // row[11] = 46;
+    // row[12] = 49;
+    // row[13] = 56;
 
-    col[0] = 1;
-    col[1] = 2;
-    col[2] = 3;
-    col[3] = 0;
-    col[4] = 2;
-    col[5] = 3;
-    col[6] = 12;
-    col[7] = 0;
-    col[8] = 1;
-    col[9] = 3;
-    col[10] = 6;
-    col[11] = 12;
-    col[12] = 0;
-    col[13] = 1;
-    col[14] = 2;
-    col[15] = 8;
-    col[16] = 12;
-    col[17] = 5;
-    col[18] = 6;
-    col[19] = 7;
-    col[20] = 12;
-    col[21] = 4;
-    col[22] = 6;
-    col[23] = 7;
-    col[24] = 9;
-    col[25] = 12;
-    col[26] = 2;
-    col[27] = 4;
-    col[28] = 5;
-    col[29] = 7;
-    col[30] = 12;
-    col[31] = 4;
-    col[32] = 5;
-    col[33] = 6;
-    col[34] = 3;
-    col[35] = 9;
-    col[36] = 10;
-    col[37] = 11;
-    col[38] = 5;
-    col[39] = 8;
-    col[40] = 10;
-    col[41] = 11;
-    col[42] = 8;
-    col[43] = 9;
-    col[44] = 11;
-    col[45] = 12;
-    col[46] = 8;
-    col[47] = 9;
-    col[48] = 10;
-    col[49] = 1;
-    col[50] = 2;
-    col[51] = 3;
-    col[52] = 4;
-    col[53] = 5;
-    col[54] = 6;
-    col[55] = 10;
+    // col[0] = 1;
+    // col[1] = 2;
+    // col[2] = 3;
+    // col[3] = 0;
+    // col[4] = 2;
+    // col[5] = 3;
+    // col[6] = 12;
+    // col[7] = 0;
+    // col[8] = 1;
+    // col[9] = 3;
+    // col[10] = 6;
+    // col[11] = 12;
+    // col[12] = 0;
+    // col[13] = 1;
+    // col[14] = 2;
+    // col[15] = 8;
+    // col[16] = 12;
+    // col[17] = 5;
+    // col[18] = 6;
+    // col[19] = 7;
+    // col[20] = 12;
+    // col[21] = 4;
+    // col[22] = 6;
+    // col[23] = 7;
+    // col[24] = 9;
+    // col[25] = 12;
+    // col[26] = 2;
+    // col[27] = 4;
+    // col[28] = 5;
+    // col[29] = 7;
+    // col[30] = 12;
+    // col[31] = 4;
+    // col[32] = 5;
+    // col[33] = 6;
+    // col[34] = 3;
+    // col[35] = 9;
+    // col[36] = 10;
+    // col[37] = 11;
+    // col[38] = 5;
+    // col[39] = 8;
+    // col[40] = 10;
+    // col[41] = 11;
+    // col[42] = 8;
+    // col[43] = 9;
+    // col[44] = 11;
+    // col[45] = 12;
+    // col[46] = 8;
+    // col[47] = 9;
+    // col[48] = 10;
+    // col[49] = 1;
+    // col[50] = 2;
+    // col[51] = 3;
+    // col[52] = 4;
+    // col[53] = 5;
+    // col[54] = 6;
+    // col[55] = 10;
 
-    val[0] = 1;    
-    val[1] = 1;    
-    val[2] = 1;    
-    val[3] = 1;    
-    val[4] = 1;    
-    val[5] = 1;    
-    val[6] = 1;
-    val[7] = 1;    
-    val[8] = 1;    
-    val[9] = 1;    
-    val[10] = 1;
-    val[11] = 1;
-    val[12] = 1;
-    val[13] = 1;
-    val[14] = 1;
-    val[15] = 1;
-    val[16] = 1;
-    val[17] = 1;
-    val[18] = 1;
-    val[19] = 1;
-    val[20] = 1;
-    val[21] = 1;
-    val[22] = 1;
-    val[23] = 1;
-    val[24] = 1;
-    val[25] = 1;
-    val[26] = 1;
-    val[27] = 1;
-    val[28] = 1;
-    val[29] = 1;
-    val[30] = 1;
-    val[31] = 1;
-    val[32] = 1;
-    val[33] = 1;
-    val[34] = 1;
-    val[35] = 1;
-    val[36] = 1;
-    val[37] = 1;
-    val[38] = 1;
-    val[39] = 1;
-    val[40] = 1;
-    val[41] = 1;
-    val[42] = 1;
-    val[43] = 1;
-    val[44] = 1;
-    val[45] = 1;
-    val[46] = 1;
-    val[47] = 1;
-    val[48] = 1;
-    val[49] = 1;
-    val[50] = 1;
-    val[51] = 1;
-    val[52] = 1;
-    val[53] = 1;
-    val[54] = 1;
-    val[55] = 1;
+    // val[0] = 1;    
+    // val[1] = 1;    
+    // val[2] = 1;    
+    // val[3] = 1;    
+    // val[4] = 1;    
+    // val[5] = 1;    
+    // val[6] = 1;
+    // val[7] = 1;    
+    // val[8] = 1;    
+    // val[9] = 1;    
+    // val[10] = 1;
+    // val[11] = 1;
+    // val[12] = 1;
+    // val[13] = 1;
+    // val[14] = 1;
+    // val[15] = 1;
+    // val[16] = 1;
+    // val[17] = 1;
+    // val[18] = 1;
+    // val[19] = 1;
+    // val[20] = 1;
+    // val[21] = 1;
+    // val[22] = 1;
+    // val[23] = 1;
+    // val[24] = 1;
+    // val[25] = 1;
+    // val[26] = 1;
+    // val[27] = 1;
+    // val[28] = 1;
+    // val[29] = 1;
+    // val[30] = 1;
+    // val[31] = 1;
+    // val[32] = 1;
+    // val[33] = 1;
+    // val[34] = 1;
+    // val[35] = 1;
+    // val[36] = 1;
+    // val[37] = 1;
+    // val[38] = 1;
+    // val[39] = 1;
+    // val[40] = 1;
+    // val[41] = 1;
+    // val[42] = 1;
+    // val[43] = 1;
+    // val[44] = 1;
+    // val[45] = 1;
+    // val[46] = 1;
+    // val[47] = 1;
+    // val[48] = 1;
+    // val[49] = 1;
+    // val[50] = 1;
+    // val[51] = 1;
+    // val[52] = 1;
+    // val[53] = 1;
+    // val[54] = 1;
+    // val[55] = 1;
 
-    row_id[0] = 0;    
-    row_id[1] = 0;    
-    row_id[2] = 0;    
-    row_id[3] = 1;    
-    row_id[4] = 1;    
-    row_id[5] = 1;    
-    row_id[6] = 1;
-    row_id[7] = 2;    
-    row_id[8] = 2;    
-    row_id[9] = 2;    
-    row_id[10] = 2;
-    row_id[11] = 2;
-    row_id[12] = 3;
-    row_id[13] = 3;
-    row_id[14] = 3;
-    row_id[15] = 3;
-    row_id[16] = 3;
-    row_id[17] = 4;
-    row_id[18] = 4;
-    row_id[19] = 4;
-    row_id[20] = 4;
-    row_id[21] = 5;
-    row_id[22] = 5;
-    row_id[23] = 5;
-    row_id[24] = 5;
-    row_id[25] = 5;
-    row_id[26] = 6;
-    row_id[27] = 6;
-    row_id[28] = 6;
-    row_id[29] = 6;
-    row_id[30] = 6;
-    row_id[31] = 7;
-    row_id[32] = 7;
-    row_id[33] = 7;
-    row_id[34] = 8;
-    row_id[35] = 8;
-    row_id[36] = 8;
-    row_id[37] = 8;
-    row_id[38] = 9;
-    row_id[39] = 9;
-    row_id[40] = 9;
-    row_id[41] = 9;
-    row_id[42] = 10;
-    row_id[43] = 10;
-    row_id[44] = 10;
-    row_id[45] = 10;
-    row_id[46] = 11;
-    row_id[47] = 11;
-    row_id[48] = 11;
-    row_id[49] = 12;
-    row_id[50] = 12;
-    row_id[51] = 12;
-    row_id[52] = 12;
-    row_id[53] = 12;
-    row_id[54] = 12;
-    row_id[55] = 12;
+    // row_id[0] = 0;    
+    // row_id[1] = 0;    
+    // row_id[2] = 0;    
+    // row_id[3] = 1;    
+    // row_id[4] = 1;    
+    // row_id[5] = 1;    
+    // row_id[6] = 1;
+    // row_id[7] = 2;    
+    // row_id[8] = 2;    
+    // row_id[9] = 2;    
+    // row_id[10] = 2;
+    // row_id[11] = 2;
+    // row_id[12] = 3;
+    // row_id[13] = 3;
+    // row_id[14] = 3;
+    // row_id[15] = 3;
+    // row_id[16] = 3;
+    // row_id[17] = 4;
+    // row_id[18] = 4;
+    // row_id[19] = 4;
+    // row_id[20] = 4;
+    // row_id[21] = 5;
+    // row_id[22] = 5;
+    // row_id[23] = 5;
+    // row_id[24] = 5;
+    // row_id[25] = 5;
+    // row_id[26] = 6;
+    // row_id[27] = 6;
+    // row_id[28] = 6;
+    // row_id[29] = 6;
+    // row_id[30] = 6;
+    // row_id[31] = 7;
+    // row_id[32] = 7;
+    // row_id[33] = 7;
+    // row_id[34] = 8;
+    // row_id[35] = 8;
+    // row_id[36] = 8;
+    // row_id[37] = 8;
+    // row_id[38] = 9;
+    // row_id[39] = 9;
+    // row_id[40] = 9;
+    // row_id[41] = 9;
+    // row_id[42] = 10;
+    // row_id[43] = 10;
+    // row_id[44] = 10;
+    // row_id[45] = 10;
+    // row_id[46] = 11;
+    // row_id[47] = 11;
+    // row_id[48] = 11;
+    // row_id[49] = 12;
+    // row_id[50] = 12;
+    // row_id[51] = 12;
+    // row_id[52] = 12;
+    // row_id[53] = 12;
+    // row_id[54] = 12;
+    // row_id[55] = 12;
 
 
-    // for(int i = 0; i < n; i++) {
-    //     for(int j = row[i]; j < row[i+1]; j++) {
-    //         row_id[j] = i;
-    //     }
-    // }
+    for(int i = 0; i < n; i++) {
+        for(int j = row[i]; j < row[i+1]; j++) {
+            row_id[j] = i;
+        }
+    }
     for(int i = 0; i < cols; i++) {
         labellist[i].key = -1;
         labellist[i].val = 0;
@@ -536,7 +536,14 @@ int main(int argc, char *argv[]) {
 
     curandState *dev_random;
     cudaMallocManaged(&dev_random, N*B*sizeof(curandState));
-
+    double times = 0;
+    double timel1 = 0;
+    double timel2 = 0;
+    double timel2_1 = 0;
+    double timel2_2 = 0;
+    double timel3 = 0;
+    double timepp = 0;
+    auto start = std::chrono::high_resolution_clock::now();
     for(int i = 1; i < T; i++) {
         //  if(i%mod == 0) {
         // std::cout << "memnnz values per vertex:\n";
@@ -549,8 +556,13 @@ int main(int argc, char *argv[]) {
         // if(DEBUGS) {
         // std::cout << time(NULL) << "\n";
         // }
+        auto starts = std::chrono::high_resolution_clock::now();
         SLPASpeaker<<<B,N>>>(row,col,val, memnnz, row_id, mem, labellist, n, T, dev_random, time(NULL)+i);
         cudaDeviceSynchronize();
+        auto ends = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diffs = ends- starts;
+        times += diffs.count();
+
         if(DEBUGS) {
             if(i%mod == 0) {
                 std::cout << "finished iteration after speaker    " << i << "\n";
@@ -564,7 +576,11 @@ int main(int argc, char *argv[]) {
             }
         }
         //label frequency
+         auto startl1 = std::chrono::high_resolution_clock::now();
         SLPAListener1<<<B,N>>>(row,col,val, labellist, row_id, n);
+             auto endl1 = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diffl1 = endl1 - startl1;
+        timel1 += diffl1.count();
         cudaDeviceSynchronize();
         if(DEBUG1) {
             if(i%mod == 0) {
@@ -579,8 +595,12 @@ int main(int argc, char *argv[]) {
             }
         }
         //max frequency
+                auto startl2 = std::chrono::high_resolution_clock::now();
         SLPAListener2<<<B,N>>>(row,col,val, labellist, n, row_id);
         cudaDeviceSynchronize();
+            auto endl2 = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diffl2 = endl2 - startl2;
+        timel2 += diffl2.count();
         if(DEBUG2) {
          if(i%mod == 0) {
             std::cout << "finished iteration after listener 2    " << i << "\n";
@@ -599,8 +619,12 @@ int main(int argc, char *argv[]) {
             labellistnnz[k] = 1;
         }
         //check for tiebreak
+        auto startl2_1 = std::chrono::high_resolution_clock::now();
         SLPAListener2_1<<<B,N>>>(row, labellist, n, row_id, labellistnnz);
         cudaDeviceSynchronize();
+        auto endl2_1 = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diffl2_1 = endl2_1 - startl2_1;
+        timel2_1 += diffl2_1.count();
         if(DEBUG2) {
          if(i%mod == 0) {
             std::cout << "finished iteration after listener 2_1   " << i << "\n";
@@ -615,8 +639,12 @@ int main(int argc, char *argv[]) {
         }
 
         //perform tiebreak
+         auto startl2_2 = std::chrono::high_resolution_clock::now();
         SLPAListener2_2<<<B,N>>>(row, labellist, n, row_id, labellistnnz, dev_random, time(NULL)+i);
         cudaDeviceSynchronize();
+        auto endl2_2 = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diffl2_2 = endl2_2 - startl2_2;
+        timel2_2 += diffl2_2.count();
         if(DEBUG2) {
          if(i%mod == 0) {
             std::cout << "finished iteration after listener 2_2    " << i << "\n";
@@ -631,8 +659,12 @@ int main(int argc, char *argv[]) {
         }
 
         //memory update
+         auto startl3 = std::chrono::high_resolution_clock::now();
         SLPAListener3<<<B,N>>>(row,col,val, memnnz, mem, labellist, n, T);
         cudaDeviceSynchronize();
+        auto endl3 = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diffl3 = endl3 - startl3;
+        timel3 += diffl3.count();
         if(DEBUG3) {
             //std::cout << "Finished iteration " << i << "\n";
             if(i%mod == 0) {
@@ -647,24 +679,45 @@ int main(int argc, char *argv[]) {
             }
         }
     }
-    
+     auto startpp = std::chrono::high_resolution_clock::now();
     SLPAPostProcess<<<B,N>>>(mem, n, T, r);
     cudaDeviceSynchronize();
+     auto endpp = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diffpp = endpp - startpp;
+    auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diff = end - start;
+        std::cout << "\nTime do total is "
+                  <<  diff.count() << " s\n";
+
+        std::cout << "average time for speaker is "
+                  <<  times/(double)T << " s\n";
+        std::cout << "average time for listerner 1 is "
+                  <<  timel1/(double)T << " s\n";
+        std::cout << "average time for listerner 2 is "
+                  <<  timel2/(double)T << " s\n";
+        std::cout << "average time for listerner 2_1 is "
+                  <<  timel2_1/(double)T << " s\n";
+        std::cout << "average time for listerner 2_2 is "
+                  <<  timel2_2/(double)T << " s\n";
+        std::cout << "average time for listerner 3 is "
+                  <<  timel3/(double)T << " s\n";
+        std::cout << "time for post is "
+                  <<  diffpp.count() << " s\n";
     //SLPA<<<B,N>>>(row,col,val,mem, n, T,r, dev_random);
-    for(int i = 0; i < n; i++) {
-        int count = 0;
-        std::cout << "Vertex " << i << " is in communities ";
-        for(int j = 0; j < memnnz[i]; j++) {
-            if(mem[i*T+j].key != -1) {
-                std::cout << mem[i*T+j].key << " ";
-                count++;
-            }
-        }
-        if(count > 1)
-            std::cout << "overlap\n";
-        else
-            std::cout << " only community\n";
-    }
-    std::cout << "\n";
+    // for(int i = 0; i < n; i++) {
+    //     int count = 0;
+    //     std::cout << "Vertex " << i << " is in communities ";
+    //     for(int j = 0; j < memnnz[i]; j++) {
+    //         if(mem[i*T+j].key != -1) {
+    //             std::cout << mem[i*T+j].key << " ";
+    //             count++;
+    //         }
+    //     }
+    //     if(count > 1)
+    //         std::cout << "overlap\n";
+    //     else
+    //         std::cout << " only community\n";
+    // }
+    // std::cout << "\n";
     return 0;
 }
